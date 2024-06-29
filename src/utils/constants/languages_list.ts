e2af8a5179b3f8f27fcc9ad44f500673c38820b2
sailesh <vermasailesh271@gmail.com>
2024-06-30 01:41:49 +0530

    added theme and language lists

---
+++ src/utils/constants/languages_list.ts
@@ -0,0 +1,18 @@
+export interface LanguageBundle {
+  language: string;
+  code: string;
+}
+
+export const LanguageBundlesList: LanguageBundle[] = [
+  { language: "JSX", code: "jsx" },
+  { language: "TSX", code: "tsx" },
+  { language: "Swift", code: "swift" },
+  { language: "Kotlin", code: "kotlin" },
+  { language: "Objective-C", code: "objectivec" },
+  { language: "Rust", code: "rust" },
+  { language: "GraphQL", code: "graphql" },
+  { language: "Yaml", code: "yaml" },
+  { language: "GO", code: "go" },
+  { language: "C++", code: "cpp" },
+  { language: "Python", code: "python" },
+];
